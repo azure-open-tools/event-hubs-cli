fb74b2a4679f3fd567a9c0c4e392b2b4c4d3adf8
Fabiano Arruda <blank@email.ok>
2020-05-02 23:39:30 +0200

    add docker to osx

--- ci/add-release-assets.sh
+++ ci/add-release-assets.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 git fetch --all
+git branch
 isMaster=$(git rev-parse --abbrev-ref HEAD)
+latestTag="$(git --no-pager tag -l | tail -1)"
+
+# git for-each-ref --sort=creatordate --format '%(creatordate)'
+echo "$PWD"
+echo "Latest Tag: $latestTag"
+#find . -type f -iname "ehs-*" -exec ls -lah {} \;
 
 if [[ "$isMaster" == *"master"* ]];
 then
-  latestTag="$(git --no-pager tag -l | tail -1)"
-  # git for-each-ref --sort=creatordate --format '%(creatordate)'
-
-  echo "Latest Tag: $latestTag"
-  #find . -type f -iname "ehs-*" -exec ls -lah {} \;
-
   # sender
   find . -type f -iname "ehs-*" -exec hub release edit -m "" -a {} "$latestTag" \;
 
