bb47191099f21d600634f1683a3a5682ecc78592
Fabiano Arruda <blank@email.ok>
2020-05-03 16:40:38 +0200

    try a workaround to get tags

--- ci/release.sh
+++ ci/release.sh
@@ -3,6 +3,22 @@
 versionFile=$1
 packageVersion=$2
 
+# workaround while we try to discover how to the get the
+# git tags within git action environment
+function getLatestTag() {
+  echo "cloning into the temp folder"
+  mkdir temp && cd temp || exit 1
+  git clone git@github.com:azure-open-tools/event-hubs-cli.git
+  cd event-hubs-cli || exit 1
+  git fetch --all
+}
+
+function deleteTempFolder() {
+  echo "deleting temp folder"
+  cd ../../
+  rm -rf temp/
+}
+
 if [[ $versionFile == *"package"* ]];
 then
   version="$packageVersion"
@@ -10,12 +26,12 @@ else
   version=$(go run "$versionFile")
 fi
 
-git fetch --all
-echo "GitHubRef: $GITHUB_REF"
+getLatestTag
 checkTag=$(git --no-pager tag -l | grep "$version" | xargs)
 if [[ $checkTag != "" ]];
 then
   echo "$checkTag already exist, skipping release."
+  deleteTempFolder
   exit 0
 fi
 
@@ -27,3 +43,5 @@ echo "Latest Tag: $latestTag"
 echo -e "Change Log Since Latest Tag: \n$changeLog"
 
 hub release create -m "Azure Event Hubs Lib $version" -m "$changeLog" "$version"
+
+deleteTempFolder
