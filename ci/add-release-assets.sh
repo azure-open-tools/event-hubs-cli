b9f4c960cc2d1d0b60e70eede85c7a2316033726
Fabiano Arruda <blank@email.ok>
2020-05-03 16:57:43 +0200

    workaround to release binaries too

--- ci/add-release-assets.sh
+++ ci/add-release-assets.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
 
-git fetch --all
-latestTag="$(git --no-pager tag -l | tail -1)"
+latestTag=""
 
+function deleteTempFolder() {
+  echo "deleting temp folder"
+  cd ../../
+  rm -rf temp/
+}
+
+# workaround while we try to discover how to the get the
+# git tags within git action environment
+function getLatestTag() {
+  echo "cloning into the temp folder"
+  mkdir temp && cd temp || exit 1
+
+  git clone https://github.com/azure-open-tools/event-hubs-cli.git
+  cd event-hubs-cli || exit 1
+
+  git fetch --all
+  latestTag="$(git --no-pager tag -l | tail -1)"
+  deleteTempFolder
+}
+
+getLatestTag
 # git for-each-ref --sort=creatordate --format '%(creatordate)'
 echo "$PWD"
 echo "Latest Tag: $latestTag"
